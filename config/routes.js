038ce6613d57a264941330652eaf18411c832b5c
taslimy <taslimer@gmail.com>
2019-06-14 12:35:53 -0400

    Abstracted Token, Login, Get list of jokes. - MVP Completed.

--- config/routes.js
+++ config/routes.js
@@ -1,46 +1,62 @@
-const axios = require('axios');
+const axios = require("axios");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
-const { authenticate } = require('../auth/authenticate');
-
+const { authenticate, generateToken } = require("../auth/authenticate");
 
 // Model
-const Actions = require('./model');
+const Actions = require("./model");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
   // implement user registration
-  let user = req.body
+  let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
 
-  Actions.add(user)
-  .then(added => {
-    res.status(200).json({added, message: "user has been registered"})
-  })
+  Actions.add(user).then(added => {
+    res.status(200).json({ added, message: "user has been registered" });
+  });
 }
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  Actions.findBy({ username })
+
+    .first()
+    .then(user => {
+      console.log(user);
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
